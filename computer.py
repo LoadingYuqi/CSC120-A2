12393756a9e7fdbe8b0afc76ee682a0b68f4bcbf
Yuqi Wang <kwang@smith.edu>
2023-02-11 05:12:14 -0500

    Start building computer class and shop class

--- computer.py
+++ computer.py
@@ -1,10 +1,28 @@
-class Computer:
+"""
+    Filename: computer.py
+    Description: An object-oriented code to create a computer for a computer resale shop.
+    Author: Yuqi Wang
+    Date: 11 February 2023
+"""
+class computer:
 
-    # What attributes will it need?
+    # Attributes of a computer in the store
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = ""
+    memory: int = ""
+    operating_system: str = ""
+    year_made: int = ""
+    price: int = ""
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # The __init__ constructs an object in the computer store
+    def __init__(self, descriptionText, processor, hardDrive, memoryInt, operatingSystem, yearMade, priceInt):
+        self.description = descriptionText
+        self.processor_type = processor
+        self.hard_drive_capacity = hardDrive
+        self.memory = memoryInt
+        self.operating_system = operatingSystem
+        self.year_made = yearMade
+        self.price = priceInt
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,45 @@
-class ResaleShop:
+"""
+    Filename: oo_resale_shop.py
+    Description: An object-oriented code to run a small computer resale shop.
+    Author: Yuqi Wang
+    Date: 11 February 2023
+"""
+from computer import *
+
+class oo_resale_shop:
 
     # What attributes will it need?
+    ItemID: int = 0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, items):
+        # Print the banner
+        print("-" * 21)
+        print("COMPUTER RESALE STORE")
+        print("-" * 21)
+        self.ItemID = items
+
+    def addComputer():
+        itemID += 1
+
+    def sellComputer():
+        itemID -= 1
+    
+    def updatePrice(self, new_price):
+        computer.price = new_price
+
+    def updateOS(self, new_OS):
+        computer.operating_system = new_OS
 
     # What methods will you need?
+
+def main(): # example of taking in a computer , processing, and selling it
+    first_computer = computer(
+        "Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 
+        64,
+        "macOS Big Sur", 
+        2013, 
+        1500) #Calling Computer class
